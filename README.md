d84df1e0bf7b4f9c906b561ad5b0a26f4ef8a668
segersniels <segers.n@hotmail.com>
2024-04-19 17:59:22 +0200

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Niels Segers
+
+Hey there! 👋 My name is Niels, and I'm a passionate full stack developer based in Belgium and father of two boys. With a keen interest in both frontend and backend technologies, I strive to master various tools and frameworks.
+Currently working at [Delta Investment Tracker](https://delta.app) as Lead Backend Developer, contributing to the growth and success of the platform.
+
+Checkout my projects [here](https://github.com/segersniels) or learn more about me at [niels.foo](https://niels.foo).
---
+++ main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/charmbracelet/bubbles/viewport"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/glamour"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/log"
+	"github.com/charmbracelet/ssh"
+	"github.com/charmbracelet/wish"
+	"github.com/charmbracelet/wish/activeterm"
+	"github.com/charmbracelet/wish/bubbletea"
+	"github.com/charmbracelet/wish/logging"
+)
+
+const (
+	host = "localhost"
+	port = "12345"
+)
+
+// Just a generic tea.Model to demo terminal information of ssh.
+type model struct {
+	viewport viewport.Model
+}
+
+func (e model) Init() tea.Cmd {
+	return nil
+}
+
+func (e model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "ctrl+c", "esc":
+			return e, tea.Quit
+		default:
+			var cmd tea.Cmd
+			e.viewport, cmd = e.viewport.Update(msg)
+			return e, cmd
+		}
+	default:
+		return e, nil
+	}
+}
+
+func (e model) View() string {
+	return e.viewport.View()
+}
+
+func teaHandler(s ssh.Session) (tea.Model, []tea.ProgramOption) {
+	width := 80
+	pty, _, _ := s.Pty()
+	vp := viewport.New(width, pty.Window.Height)
+	vp.Style = lipgloss.NewStyle().
+		BorderStyle(lipgloss.RoundedBorder()).
+		BorderForeground(lipgloss.Color("62")).
+		AlignHorizontal(lipgloss.Center)
+
+	renderer, _ := glamour.NewTermRenderer(
+		glamour.WithAutoStyle(),
+		glamour.WithWordWrap(width),
+	)
+
+	// Read from local ./README.md file
+	content, _ := os.ReadFile("./README.md")
+	str, _ := renderer.Render(string(content))
+	vp.SetContent(str)
+
+	m := model{
+		viewport: vp,
+	}
+
+	return m, []tea.ProgramOption{tea.WithAltScreen()}
+}
+
+func main() {
+	s, err := wish.NewServer(
+		wish.WithAddress(net.JoinHostPort(host, port)),
+		wish.WithHostKeyPath(".ssh/id_ed25519"),
+		wish.WithMiddleware(
+			bubbletea.Middleware(teaHandler),
+			activeterm.Middleware(), // Bubble Tea apps usually require a PTY.
+			logging.Middleware(),
+		),
+	)
+
+	if err != nil {
+		log.Error("Could not start server", "error", err)
+	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	log.Info("Starting SSH server", "host", host, "port", port)
+	go func() {
+		if err = s.ListenAndServe(); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+			log.Error("Could not start server", "error", err)
+			done <- nil
+		}
+	}()
+
+	<-done
+	log.Info("Stopping SSH server")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer func() { cancel() }()
+	if err := s.Shutdown(ctx); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+		log.Error("Could not stop server", "error", err)
+	}
+}
